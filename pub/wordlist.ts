c6f344c42684c4e86d2204fae26823c2b6645463
sholvoir <sovar.he@gmail.com>
2025-02-28 12:56:35 -0500

    0.6.3

--- pub/wordlist.ts
+++ pub/wordlist.ts
@@ -1,24 +1,13 @@
 import { Hono } from "hono";
-import { emptyResponse, STATUS_CODE } from "@sholvoir/generic/http";
 import { collectionWordList } from "../lib/mongo.ts";
 
 const app = new Hono();
 
 app.get(async (c) => {
-    const wlid = c.req.query('wlid');
-    if (!wlid) return emptyResponse(STATUS_CODE.BadRequest);
-    const wl = await collectionWordList.findOne({ wlid });
-    if (!wl) return emptyResponse(STATUS_CODE.NotFound);
-    console.log(`API wordlist GET ${wlid}`);
-    return c.json(wl);
-}).post(async (c) => {
-    const wlids = await c.req.json() as Array<string>;
-    if (!Array.isArray(wlids))
-        return emptyResponse(STATUS_CODE.BadRequest);
-    const wls = [];
-    for await (const wl of collectionWordList.find({ wlid: { $in: wlids } }))
-        wls.push(wl);
-    console.log(`API wordlist POST`);
+    const wls = []
+    for await (const wl of collectionWordList.find())
+        wls.push(wl)
+    console.log(`API wordlist GET`);
     return c.json(wls);
 });
 
