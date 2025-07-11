19894336ad414e266e917690f6399a544fda321e
sholvoir <sovar.he@gmail.com>
2025-07-11 18:32:42 -0400

    0.9.1

--- pub/dict.ts
+++ pub/dict.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { emptyResponse, STATUS_CODE } from '@sholvoir/generic/http';
 import { collectionDict } from "../lib/mongo.ts";
 import { getVocabulary } from "../lib/spell-check.ts";
+import { now } from "../lib/common.ts";
 import fill from '../lib/fill-dict.ts';
 
 const app = new Hono();
@@ -13,7 +14,7 @@ app.get(async (c) => {
     if (dict) return c.json(dict)
     const card = await fill(word, {});
     const vocab = await getVocabulary();
-    const ndict = { word, cards: [card] };
+    const ndict = { word, cards: [card], version: now() };
     if (vocab.has(word)) await collectionDict.insertOne(ndict);
     console.log(`API 'dict' GET word: ${word}`);
     return c.json(ndict);
