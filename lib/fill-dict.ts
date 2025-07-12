e2cb43c29025c43ebadbd8b0623cbc957811234e
sholvoir <sovar.he@gmail.com>
2025-07-12 01:51:06 -0400

    0.9.3

--- lib/fill-dict.ts
+++ lib/fill-dict.ts
@@ -8,16 +8,12 @@ import youdao from "./youdao.ts";
 const fillDict = async (word: string, card: ICard) => {
     // Webster-api //if (!card.sound) 
     await websterApi(word, card);
-    console.log(1, card)
     // Webster-web
     if (!card.sound) await websterWeb(word, card);
-    console.log(2, card)
     // Oxford //if (!card.phonetic || !card.sound) 
     await oxford(word, card);
-    console.log(3, card)
     // Google Dictionary //(!card.sound || !card.phonetic || !card.def)
     await dictionary(word, card);
-    console.log(4, card)
     // Youdao // (!card.sound || !card.phonetic || !card.def)
     await youdao(word, card);
     return card;
