65a5064cd641978d1a685678a6f170e5cd667e5e
sholvoir <sovar.he@gmail.com>
2025-08-12 19:32:59 -0400

    0.13.0-0.18.0

--- main.ts
+++ main.ts
@@ -31,6 +31,9 @@ const run = async () => {
     const app = new Hono();
     app.use(cors());
     app.use(serveStatic({root: './static/'}));
+    app.use('/assets/*', serveStatic({root: './assets/'}));
+    app.get('/', async (c) => c.html(await Deno.readTextFile('./html/index.html')));   
+    app.get('/admin', async (c) => c.html(await Deno.readTextFile('./html/admin.html'))); 
 
     app.route('/api/v2/ecdict-as-issue', ecdictAsIssue);
 
