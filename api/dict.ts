e5ba0c1e1d426061d72c2004113e6b18f2a92974
sholvoir <sovar.he@gmail.com>
2025-08-13 09:20:12 -0400

    0.13.6-0.18.5

--- api/dict.ts
+++ api/dict.ts
@@ -4,10 +4,26 @@ import { IDict } from "@sholvoir/memword-common/idict";
 import { now } from "@sholvoir/memword-common/common";
 import { jwtEnv } from "../lib/env.ts";
 import { collectionDict } from "../lib/mongo.ts";
+import { getVocabulary } from "../lib/spell-check.ts";
+import fill from '../lib/fill-dict.ts';
+import user from "../mid/user.ts";
+import auth from "../mid/auth.ts";
+import admin from "../mid/admin.ts";
 
 const app = new Hono<jwtEnv>();
 
-app.put(async (c) => {
+app.get(async (c) => {
+    const word = c.req.query('q');
+    if (!word) return emptyResponse(STATUS_CODE.BadRequest);
+    const dict = await collectionDict.findOne({ word });
+    if (dict) return c.json(dict)
+    const entry = await fill(word, {});
+    const vocab = await getVocabulary();
+    const ndict: IDict = { word, entries: [entry], version: now() };
+    if (vocab.has(word)) await collectionDict.insertOne(ndict);
+    console.log(`API 'dict' GET word: ${word}`);
+    return c.json(ndict);
+}).put(user, auth, admin, async (c) => {
     const clientDict = await c.req.json() as IDict;
     if (!clientDict) return emptyResponse(STATUS_CODE.BadRequest);
     delete clientDict._id;
@@ -16,7 +32,7 @@ app.put(async (c) => {
     if (!result.acknowledged) return emptyResponse(STATUS_CODE.InternalServerError);
     console.log(`API dict PUT ${clientDict.word}`);
     return emptyResponse();
-}).delete(async (c) => {
+}).delete(user, auth, admin, async (c) => {
     const word = c.req.query('q');
     if (!word) return emptyResponse(STATUS_CODE.BadRequest);
     const result = await collectionDict.deleteOne({ word });
--- main.ts
+++ main.ts
@@ -10,7 +10,6 @@ import pub_signup from "./pub/signup.ts";
 import pub_signin from "./pub/signin.ts";
 import pub_otp from "./pub/otp.ts";
 import pub_sound from "./pub/sound.ts";
-import pub_dict from "./pub/dict.ts";
 import pub_book from "./pub/book.ts";
 import pub_vocabulary from "./pub/vocabulary.ts";
 import pub_definition from "./pub/definition.ts";
@@ -21,13 +20,13 @@ import auth_issue from './auth/issue.ts';
 import auth_setting from './auth/setting.ts';
 import auth_book from "./auth/book.ts";
 
-import admin_dict from "./admin/dict.ts";
 import admin_issue from "./admin/issue.ts";
 import admin_vocabulary from "./admin/vocabulary.ts";
 
 import ecdictAsIssue from './api/ecdict-as-issue.ts';
 
 import { connect } from './lib/mongo.ts';
+import dict from "./api/dict.ts";
 
 const run = async () => {
     const app = new Hono();
@@ -36,13 +35,13 @@ const run = async () => {
     app.get('/', user, cookie, serveStatic({ path:'./html/index.html' }));
     app.get('/admin', user, auth, cookie, serveStatic({ path: './html/admin.html' }));
 
+    app.route('/api/v2/dict', dict)
     app.route('/api/v2/ecdict-as-issue', ecdictAsIssue);
 
     app.route('/pub/signup', pub_signup);
     app.route('/pub/signin', pub_signin);
     app.route('/pub/otp', pub_otp);
     app.route('/pub/sound', pub_sound);
-    app.route('/pub/dict', pub_dict);
     app.route('/pub/book', pub_book);
     app.route('/pub/vocabulary', pub_vocabulary);
     app.route('/pub/definition', pub_definition);
@@ -55,7 +54,6 @@ const run = async () => {
     app.route('/api/v1/book', auth_book);
 
     app.use('/admin/*', user, auth, admin);
-    app.route('/admin/dict', admin_dict);
     app.route('/admin/issue', admin_issue);
     app.route('/admin/vocabulary', admin_vocabulary);
 
