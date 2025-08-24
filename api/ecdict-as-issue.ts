f8b9033434fe240bbd88c77bd8f2df3d29c8e78e
sholvoir <sovar.he@gmail.com>
2025-08-24 09:30:02 -0400

    0.14.3-0.19.5

--- api/ecdict-as-issue.ts
+++ api/ecdict-as-issue.ts
@@ -1,25 +1,39 @@
 import { Hono } from "hono";
+import { collectionDict, collectionIssue } from "../lib/mongo.ts";
+import { IIssue } from "@sholvoir/memword-common/iissue";
 import user from '../mid/user.ts';
 import auth from '../mid/auth.ts';
 import admin from '../mid/admin.ts';
 
-import { collectionDict, collectionIssue } from "../lib/mongo.ts";
-import { IIssue } from "@sholvoir/memword-common/iissue";
-import { InsertManyResult } from "mongodb";
-
 const app = new Hono();
 
 app.get(user, auth, admin, async (c) => {
-    const result: InsertManyResult<IIssue> = {
-        acknowledged: true,
-        insertedIds: {},
-        insertedCount: 0
+    const issues: Array<IIssue> = [];
+    const cursor = collectionDict.find();
+    u: for await (const dict of cursor) {
+        if (dict.entries) for (const entry of dict.entries) {
+            if (entry.phonetic?.includes('/,/')) {
+                issues.push({reporter: "hua", issue: dict.word});
+                continue u;
+            }
+            if (entry.meanings) {
+                if (entry.meanings.ecdict) {
+                    issues.push({reporter: "hua", issue: dict.word});
+                    continue u;
+                }
+                for (const meaning of Object.values(entry.meanings)) {
+                    for (const mean of meaning) {
+                        if (mean.trans?.includes('""')) {
+                            issues.push({reporter: "hua", issue: dict.word});
+                            continue u;
+                        }
+                    }
+                }
+            }
+        }
     }
-    const cursor = collectionDict.find({'entries.0.meanings.ecdict': { $exists: true }});
-    for await (const dict of cursor) {
-        const r = await collectionIssue.insertOne({reporter: "hua", issue: dict.word});
-        result.insertedIds[result.insertedCount++] = r.insertedId;
-    }
+    const result = await collectionIssue.insertMany(issues);
+    if (!result.acknowledged) return c.json(result, 500);
     console.log(`API ecdict as issue GET ${result.insertedCount}`);
     return c.json(result);
 })
