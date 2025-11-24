bb6f4bd8f06599bc94ae10c666a917e2b507cb68
sholvoir <sovar.he@gmail.com>
2025-11-24 16:52:11 -0500

    0.14.8-0.19.11

--- api/ecdict-as-issue.ts
+++ api/ecdict-as-issue.ts
@@ -4,6 +4,7 @@ import { IIssue } from "@sholvoir/memword-common/iissue";
 import user from '../mid/user.ts';
 import auth from '../mid/auth.ts';
 import admin from '../mid/admin.ts';
+import { emptyResponse } from "@sholvoir/generic/http";
 
 const app = new Hono();
 
@@ -13,7 +14,7 @@ app.get(user, auth, admin, async (c) => {
     u: for await (const dict of cursor) {
         if (dict.entries) for (const entry of dict.entries) {
             if (entry.phonetic?.includes('/,/')) {
-                issues.push({reporter: "hua", issue: dict.word});
+                issues.push({ reporter: "hua", issue: dict.word });
                 continue u;
             }
             if (entry.meanings) {
@@ -23,8 +24,8 @@ app.get(user, auth, admin, async (c) => {
                 // }
                 for (const meaning of Object.values(entry.meanings)) {
                     for (const mean of meaning) {
-                        if (mean.trans?.includes('""')) {
-                            issues.push({reporter: "hua", issue: dict.word});
+                        if (`${mean.trans ?? ''}`.includes('""')) {
+                            issues.push({ reporter: "hua", issue: dict.word });
                             continue u;
                         }
                     }
@@ -32,6 +33,7 @@ app.get(user, auth, admin, async (c) => {
             }
         }
     }
+    if (!issues.length) return emptyResponse();
     const result = await collectionIssue.insertMany(issues);
     if (!result.acknowledged) return c.json(result, 500);
     console.log(`API ecdict as issue GET ${result.insertedCount}`);
