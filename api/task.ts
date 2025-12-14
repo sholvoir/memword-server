afc2d251cc79e9271515fa9d09a5fb915e02aa9c
sholvoir <sovar.he@gmail.com>
2025-12-14 16:00:05 -0500

    0.14.10-0.19.16

--- api/task.ts
+++ api/task.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import { Int32 } from "mongodb";
+import { Collection, Int32 } from "mongodb";
 import { emptyResponse, jsonResponse, STATUS_CODE } from '@sholvoir/generic/http';
 import { ITask } from "@sholvoir/memword-common/itask";
 import { getCollectionTask } from "../lib/mongo.ts";
@@ -9,37 +9,47 @@ import user from "../mid/user.ts";
 import auth from "../mid/auth.ts";
 
 const app = new Hono<jwtEnv>();
-const tasks = new Map<string, ITask>();
+const tasksMap = new Map<string, Map<string, ITask>>();
+const syncTasks = async (username: string, collectionTask: Collection<ITask>) => {
+    let serverTasks = tasksMap.get(username);
+    if (!serverTasks) {
+        serverTasks = new Map<string, ITask>();
+        tasksMap.set(username, serverTasks);
+        for await (const task of collectionTask.find())
+            serverTasks.set(task.word, task);
+    }
+    return serverTasks;
+}
 
 app.post(user, auth, async (c) => {
     const username = c.get('username');
     const collectionTask = getCollectionTask(username);
-    if (tasks.size == 0)
-        for await (const task of collectionTask.find())
-            tasks.set(task.word, task);
+    const serverTasks = await syncTasks(username, collectionTask);
     const clientTasks: Array<ITask> = await c.req.json();
     for (const ctask of clientTasks) {
         delete ctask._id;
-        if (tasks.has(ctask.word)) {
-            const stask = tasks.get(ctask.word)!;
+        if (serverTasks.has(ctask.word)) {
+            const stask = serverTasks.get(ctask.word)!;
             if (ctask.last > stask.last) {
-                tasks.set(ctask.word, ctask);
+                serverTasks.set(ctask.word, ctask);
                 const $set = { last: new Int32(ctask.last), next: new Int32(ctask.next), level: new Int32(ctask.level) } as any;
                 await collectionTask.updateOne({ word: ctask.word }, { $set });
             }
         } else {
-            tasks.set(ctask.word, ctask);
+            serverTasks.set(ctask.word, ctask);
             await collectionTask.insertOne(ctask);
         }
     }
-    console.log(`API task POST ${username} with tasks ${clientTasks.length}, return ${tasks.size}.`);
-    return jsonResponse(Array.from(tasks.values().map(task=>(delete task._id, task))));
+    console.log(`API task POST ${username} with tasks ${clientTasks.length}, return ${serverTasks.size}.`);
+    return jsonResponse(Array.from(serverTasks.values().map(task=>(delete task._id, task))));
 }).delete(user, auth, async (c) => {
     const username = c.get('username');
     const words: Array<string> = await c.req.json();
+    const collectionTask = getCollectionTask(username);
+    const serverTasks = await syncTasks(username, collectionTask);
     if (!Array.isArray(words)) return emptyResponse(STATUS_CODE.BadRequest);
-    for (const word of words) tasks.delete(word);
-    const deleteResult = await getCollectionTask(username).deleteMany({ word: { $in: words } });
+    for (const word of words) serverTasks.delete(word);
+    const deleteResult = await collectionTask.deleteMany({ word: { $in: words } });
     console.log(`API task DELETE ${username} with tasks ${deleteResult.deletedCount}.`);
     return jsonResponse(deleteResult);
 })
