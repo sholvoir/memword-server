f02006bc10add1636a1468b80e4bb850e481d53b
sholvoir <sovar.he@gmail.com>
2026-02-10 00:00:55 -0500

    0.18.1

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,10 @@
 import type { Hono } from "hono";
 import auth from "../mid/auth.ts";
-import cookie from "../mid/cookie.ts";
 
 const url = "https://www.micinfotech.com/memword";
 
 const apply = (app: Hono) => {
-   app.get("/", auth, cookie, async (c) => {
-      const res = await fetch(`${url}/index.html`);
-      if (!res.ok) return res;
-      const text = await res.text();
-      const username = c.get("username");
-      return c.html(text.replace("{{username}}", username));
-   });
+   app.get("/", auth, () => fetch(`${url}/index.html`));
    app.get("/about", () => fetch(`${url}/about.html`));
    app.get("/manifest.json", () => fetch(`${url}/manifest.json`));
    app.get("/assets/*", (c) => fetch(`${url}${c.req.path}`));
