da5ad8797430134720b15f182b91c760d078a541
sholvoir <sovar.he@gmail.com>
2026-03-28 15:25:09 -0400

    0.18.7

--- src/lib/itask.ts
+++ src/lib/itask.ts
@@ -1,21 +1,6 @@
-export const MAX_NEXT = 2000000000000;
-
 export interface ITask {
    word: string;
    last: number;
    next: number;
    level: number;
 }
-
-export const studyTask = (task: ITask, level?: number): ITask => {
-   if (level === undefined) level = ++task.level;
-   if (level > 15) task.level = level = 15;
-   if (level <= 0) task.level = level = 1;
-   const now = Date.now();
-   task.last = now;
-   task.next =
-      level >= 15
-         ? MAX_NEXT
-         : now + Math.round(39000 * level ** 3 * 1.5 ** level);
-   return task;
-};
