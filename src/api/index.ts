98399a5f7c48010329eb4d4e7191904ffc1916d8
sholvoir <sovar.he@gmail.com>
2026-04-14 19:00:01 -0400

    0.19.4

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import type { Hono } from "hono";
+import pkg from "../../deno.json" with { type: "json" };
 import auth from "../mid/auth.ts";
 import renew from "../mid/renew.ts";
 
@@ -7,6 +8,7 @@ const url = "https://www.micinfotech.com/memword";
 const apply = (app: Hono) => {
    app.get("/", auth, renew, () => fetch(`${url}/index.html`));
    app.get("/about", () => fetch(`${url}/about.html`));
+   app.get("/version", (c) => c.text(pkg.version));
    app.get("/manifest.json", () => fetch(`${url}/manifest.json`));
    app.get("/assets/*", (c) => fetch(`${url}${c.req.path}`));
    app.get("/icon/*", (c) => fetch(`${url}${c.req.path}`));
