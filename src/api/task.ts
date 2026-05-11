3803810b3038b86fd667f746830827d23fbde68a
sholvoir <sovar.he@gmail.com>
2026-05-11 02:58:37 -0400

    0.26.0

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,8 +1,4 @@
-import {
-   emptyResponse,
-   jsonResponse,
-   STATUS_CODE,
-} from "@sholvoir/generic/http";
+import { emptyResponse, STATUS_CODE } from "@sholvoir/generic/http";
 import { Hono } from "hono";
 import type { jwtEnv } from "../lib/env.ts";
 import type { ITask } from "../lib/itask.ts";
@@ -10,16 +6,27 @@ import { getCollectionTask } from "../lib/mongo.ts";
 import auth from "../mid/auth.ts";
 
 export default new Hono<jwtEnv>()
-   .post(auth, async (c) => {
+   .get(auth, async (c) => {
       const username = c.get("username");
-      const sync = c.req.query("sync");
+      const collectionTask = getCollectionTask(username);
+      const tasks = [];
+      for await (const task of collectionTask.find(
+         {},
+         { projection: { _id: 0 } },
+      ))
+         tasks.push(task);
+      console.log(`API task GET ${username} return ${tasks.length}.`);
+      return c.json(tasks);
+   })
+   .patch(auth, async (c) => {
+      const username = c.get("username");
       const clientTasks: Array<ITask> = await c.req.json();
       if (!clientTasks || !Array.isArray(clientTasks))
          return emptyResponse(STATUS_CODE.BadRequest);
       const collectionTask = getCollectionTask(username);
       const serverTasks = new Map<string, ITask>();
       for await (const task of collectionTask.find(
-         sync ? {} : { word: { $in: clientTasks.map((t) => t.word) } },
+         { word: { $in: clientTasks.map((t) => t.word) } },
          { projection: { _id: 0 } },
       ))
          serverTasks.set(task.word, task);
@@ -29,7 +36,6 @@ export default new Hono<jwtEnv>()
             const r = await collectionTask.insertOne(ctask);
             if (!r.acknowledged)
                return emptyResponse(STATUS_CODE.InternalServerError);
-            serverTasks.set(ctask.word, ctask);
          } else if (ctask.last > stask.last) {
             const r = await collectionTask.updateOne(
                { word: ctask.word },
@@ -43,17 +49,10 @@ export default new Hono<jwtEnv>()
             );
             if (!r.acknowledged)
                return emptyResponse(STATUS_CODE.InternalServerError);
-            serverTasks.set(ctask.word, ctask);
          }
       }
-      console.log(
-         `API task POST ${username} upload ${clientTasks.length}, ${
-            sync ? `return ${serverTasks.size}` : "Without Sync"
-         }.`,
-      );
-      return sync
-         ? jsonResponse(Array.from(serverTasks.values()))
-         : emptyResponse();
+      console.log(`API task PATCH ${username} upload ${clientTasks.length}.`);
+      return emptyResponse();
    })
    .delete(auth, async (c) => {
       const username = c.get("username");
