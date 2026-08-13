b939a906c5b79cec144d12054e27b6d09c354ebd
sholvoir <sovar.he@gmail.com>
2026-08-13 17:07:16 -0400

    0.27.2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,13 @@
 import type { Hono } from "hono";
+import { proxy } from "hono/proxy";
 
 const url = "https://www.micinfotech.com/memword";
 
 const apply = (app: Hono<any>) => {
-   app.get("/", () => fetch(`${url}/index.html`));
-   app.get("/manifest.json", () => fetch(`${url}/manifest.json`));
-   app.get("/assets/*", (c) => fetch(`${url}${c.req.path}`));
-   app.get("/icon/*", (c) => fetch(`${url}${c.req.path}`));
+   app.get("/", () => proxy(`${url}/index.html`));
+   app.get("/manifest.json", () => proxy(`${url}/manifest.json`));
+   app.get("/assets/*", (c) => proxy(`${url}${c.req.path}`));
+   app.get("/icon/*", (c) => proxy(`${url}${c.req.path}`));
 };
 
 export default apply;
